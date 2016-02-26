1c49b91ab8aae8a6fbcba7d94e6bd5327e1ed6bc
jaronoff97 <jacobaronoff45@gmail.com>
2016-02-26 11:10:01 -0500

    Latest Commit from sublime text

--- public/main.js
+++ public/main.js
@@ -28,7 +28,7 @@ function main() {
     // create, initiate and append game canvas
     username = window.prompt("Enter a username", "Username");
     init(); // initiate game objects
-    $(document).keydown(keyDown);
+    $(document).keydown(keyDown); //add keylisteners
     $(document).keyup(keyUp);
     draw();
 }
