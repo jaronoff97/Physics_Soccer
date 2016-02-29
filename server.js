313a8ec1aafb56225789e56bbc06bb6c086cc58c
jaronoff97 <jacobaronoff45@gmail.com>
2016-02-29 08:46:13 -0500

    Latest Commit from sublime text

--- server.js
+++ server.js
@@ -142,10 +142,6 @@ io.on('connection', function(socket) {
 
         }
     }
-
-    function intersects(r1, r2) {
-        return !(r2.left > r1.right || r2.right < r1.left || r2.top > r1.bottom || r2.bottom < r1.top);
-    }
     setInterval(moveBall, 20);
     socket.on('disconnect', function() {
         if (addedUser) {
