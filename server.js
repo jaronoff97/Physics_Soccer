a62b1c1633eb43d1020070d5e1ed02644fb14f82
jaronoff97 <jacobaronoff45@gmail.com>
2016-02-29 08:47:45 -0500

    Latest Commit from sublime text

--- server.js
+++ server.js
@@ -138,12 +138,13 @@ io.on('connection', function(socket) {
     }
 
     function checkGoalIntersections() {
-        if (ball.xpos){
+        if (ball.xpos) {
 
         }
     }
-    function rectangle_collision(float x_1, float y_1, float width_1, float height_1, float x_2, float y_2, float width_2, float height_2){
-                            return !(x_1 > x_2+width_2 || x_1+width_1 < x_2 || y_1 > y_2+height_2 || y_1+height_1 < y_2)
+
+    function rectangle_collision(float x_1, float y_1, float width_1, float height_1, float x_2, float y_2, float width_2, float height_2) {
+        return !(x_1 > x_2 + width_2 || x_1 + width_1 < x_2 || y_1 > y_2 + height_2 || y_1 + height_1 < y_2)
     }
     setInterval(moveBall, 20);
     socket.on('disconnect', function() {
